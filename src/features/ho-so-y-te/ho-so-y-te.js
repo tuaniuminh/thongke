305527b58d8e2f3c5b616eb82bcf7eeed1ccece7
tuaniuminh <tuaniuminh@users.noreply.github.com>
2026-07-18 22:47:38 +0700

    Cap nhat code tu dong

--- src/features/ho-so-y-te/ho-so-y-te.js
+++ src/features/ho-so-y-te/ho-so-y-te.js
@@ -1,8 +1,8 @@
 import { 
     state, saveLocalState, showToast, performSync,
     APP_VERSION, formatDate, escapeHTML, getLocalDateString
-} from '../../core/app.js?v=4.2.38';
-import { encrypt, decrypt } from '../../core/crypto.js?v=4.2.38';
+} from '../../core/app.js?v=4.2.39';
+import { encrypt, decrypt } from '../../core/crypto.js?v=4.2.39';
 
 let healthTrendChartInstance = null;
 
--- src/features/quy-gia-dinh/quy-gia-dinh.js
+++ src/features/quy-gia-dinh/quy-gia-dinh.js
@@ -4,9 +4,9 @@ import {
     state, saveLocalState, showToast, performSync,
     formatDate, escapeHTML, formatVND, generateId,
     decryptWithPrivateKey, loadLocalState, getLocalDateString
-} from '../../core/app.js?v=4.2.38';
-import { decrypt } from '../../core/crypto.js?v=4.2.38';
-import * as sync from '../../core/sync.js?v=4.2.38';
+} from '../../core/app.js?v=4.2.39';
+import { decrypt } from '../../core/crypto.js?v=4.2.39';
+import * as sync from '../../core/sync.js?v=4.2.39';
 
 let fundContributionChart = null;
 let fundDetailsChartsMap = {};
