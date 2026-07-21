524020479a50a71d0160747402a2028e348c9052
tuaniuminh <tuaniuminh@users.noreply.github.com>
2026-07-21 18:19:55 +0700

    Cap nhat code tu dong

--- src/features/quy-gia-dinh/quy-gia-dinh.js
+++ src/features/quy-gia-dinh/quy-gia-dinh.js
@@ -4,9 +4,9 @@ import {
     state, saveLocalState, showToast, performSync,
     formatDate, escapeHTML, formatVND, generateId,
     decryptWithPrivateKey, loadLocalState, getLocalDateString
-} from '../../core/app.js?v=4.3.13';
-import { decrypt } from '../../core/crypto.js?v=4.3.13';
-import * as sync from '../../core/sync.js?v=4.3.13';
+} from '../../core/app.js?v=4.3.14';
+import { decrypt } from '../../core/crypto.js?v=4.3.14';
+import * as sync from '../../core/sync.js?v=4.3.14';
 
 let fundContributionChart = null;
 let fundDetailsChartsMap = {};
--- src/features/thu-chi-doi-ngoai/thu-chi.js
+++ src/features/thu-chi-doi-ngoai/thu-chi.js
@@ -4,10 +4,10 @@ import {
     parseAmountInput, switchTab, getSupabaseConfig, checkLoginStatus,
     renderDashboardSyncBanner, updateHomeWeather, updateHomeLunar,
     compareRecordsByRecent, renderAll, getLocalDateString, clearAllStateData
-} from '../../core/app.js?v=4.3.13';
-import * as sync from '../../core/sync.js?v=4.3.13';
-import { encrypt, decrypt } from '../../core/crypto.js?v=4.3.13';
-import { updateLoveWidgetUI } from '../we-love/we-love.js?v=4.3.13';
+} from '../../core/app.js?v=4.3.14';
+import * as sync from '../../core/sync.js?v=4.3.14';
+import { encrypt, decrypt } from '../../core/crypto.js?v=4.3.14';
+import { updateLoveWidgetUI } from '../we-love/we-love.js?v=4.3.14';
 
 let lastDeletedRecord = null;
 let relationshipChart = null;
