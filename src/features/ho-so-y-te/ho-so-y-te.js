fb1c7ead98717f00da3f941fffbf28b5217b6d68
tuaniuminh <tuaniuminh@users.noreply.github.com>
2026-07-30 11:12:20 +0700

    Cap nhat code tu dong

--- src/features/ho-so-y-te/ho-so-y-te.js
+++ src/features/ho-so-y-te/ho-so-y-te.js
@@ -1,8 +1,8 @@
 import { 
     state, saveLocalState, showToast, performSync,
     APP_VERSION, formatDate, escapeHTML, getLocalDateString
-} from '../../core/app.js?v=4.3.42';
-import { encrypt, decrypt } from '../../core/crypto.js?v=4.3.42';
+} from '../../core/app.js?v=4.3.43';
+import { encrypt, decrypt } from '../../core/crypto.js?v=4.3.43';
 
 let healthTrendChartInstance = null;
 
