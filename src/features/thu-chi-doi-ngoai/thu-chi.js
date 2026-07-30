6cf72466d913b0c22c49d4d7cb5bbc3e9ee40f31
tuaniuminh <tuaniuminh@users.noreply.github.com>
2026-07-30 12:02:48 +0700

    Cap nhat code tu dong

--- src/features/thu-chi-doi-ngoai/thu-chi.js
+++ src/features/thu-chi-doi-ngoai/thu-chi.js
@@ -5,10 +5,10 @@ import {
     renderDashboardSyncBanner, updateHomeWeather, updateHomeLunar,
     compareRecordsByRecent, renderAll, getLocalDateString, clearAllStateData,
     updateSidebarNavVisibility
-} from '../../core/app.js?v=4.3.45';
-import * as sync from '../../core/sync.js?v=4.3.45';
-import { encrypt, decrypt } from '../../core/crypto.js?v=4.3.45';
-import { updateLoveWidgetUI } from '../we-love/we-love.js?v=4.3.45';
+} from '../../core/app.js?v=4.3.46';
+import * as sync from '../../core/sync.js?v=4.3.46';
+import { encrypt, decrypt } from '../../core/crypto.js?v=4.3.46';
+import { updateLoveWidgetUI } from '../we-love/we-love.js?v=4.3.46';
 
 let lastDeletedRecord = null;
 let relationshipChart = null;
