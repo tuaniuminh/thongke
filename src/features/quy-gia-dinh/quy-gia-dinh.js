1f59445149d48b32b81cd75d1d5155d5ffa4be2d
tuaniuminh <tuaniuminh@users.noreply.github.com>
2026-08-10 04:39:08 +0700

    Cap nhat code tu dong

--- src/features/quy-gia-dinh/quy-gia-dinh.js
+++ src/features/quy-gia-dinh/quy-gia-dinh.js
@@ -4,9 +4,9 @@ import {
     state, saveLocalState, showToast, performSync,
     formatDate, escapeHTML, formatVND, generateId,
     decryptWithPrivateKey, loadLocalState, getLocalDateString
-} from '../../core/app.js?v=4.5.8';
-import { decrypt } from '../../core/crypto.js?v=4.5.8';
-import * as sync from '../../core/sync.js?v=4.5.8';
+} from '../../core/app.js?v=4.5.9';
+import { decrypt } from '../../core/crypto.js?v=4.5.9';
+import * as sync from '../../core/sync.js?v=4.5.9';
 
 let fundContributionChart = null;
 let fundDetailsChartsMap = {};
