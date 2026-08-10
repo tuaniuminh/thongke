f946d2e94b3120dfaba33298a2f18320c324a7dd
tuaniuminh <tuaniuminh@users.noreply.github.com>
2026-08-10 20:58:32 +0700

    Optimize font size and pulsing heart size on mobile for v4.3.140

--- src/features/ket-noi-gia-dinh/ket-noi.js
+++ src/features/ket-noi-gia-dinh/ket-noi.js
@@ -2,9 +2,9 @@
 import { 
     state, saveLocalState, showToast, performSync,
     escapeHTML, decryptWithPrivateKey
-} from '../../core/app.js?v=4.3.139';
-import { encrypt, decrypt } from '../../core/crypto.js?v=4.3.139';
-import * as sync from '../../core/sync.js?v=4.3.139';
+} from '../../core/app.js?v=4.3.140';
+import { encrypt, decrypt } from '../../core/crypto.js?v=4.3.140';
+import * as sync from '../../core/sync.js?v=4.3.140';
 
 let _pairingInterval = null;
 let _pairingRealtimeChannel = null;
