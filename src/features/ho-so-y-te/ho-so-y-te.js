ce47ae06f6a3716a442fda50c45b49a9e8a091af
tuaniuminh <tuaniuminh@users.noreply.github.com>
2026-08-15 20:10:37 +0700

    Cap nhat code tu dong

--- src/features/ho-so-y-te/ho-so-y-te.js
+++ src/features/ho-so-y-te/ho-so-y-te.js
@@ -1,8 +1,8 @@
 import { 
     state, saveLocalState, showToast, performSync,
     APP_VERSION, formatDate, escapeHTML, getLocalDateString
-} from '../../core/app.js?v=4.3.185';
-import { encrypt, decrypt } from '../../core/crypto.js?v=4.3.185';
+} from '../../core/app.js?v=4.3.186';
+import { encrypt, decrypt } from '../../core/crypto.js?v=4.3.186';
 
 let healthTrendChartInstance = null;
 
--- src/features/thu-chi-doi-ngoai/thu-chi.js
+++ src/features/thu-chi-doi-ngoai/thu-chi.js
@@ -5,10 +5,10 @@ import {
     renderDashboardSyncBanner, updateHomeWeather, updateHomeLunar,
     compareRecordsByRecent, renderAll, getLocalDateString, clearAllStateData,
     updateSidebarNavVisibility
-} from '../../core/app.js?v=4.3.185';
-import * as sync from '../../core/sync.js?v=4.3.185';
-import { encrypt, decrypt } from '../../core/crypto.js?v=4.3.185';
-import { updateLoveWidgetUI } from '../we-love/we-love.js?v=4.3.185';
+} from '../../core/app.js?v=4.3.186';
+import * as sync from '../../core/sync.js?v=4.3.186';
+import { encrypt, decrypt } from '../../core/crypto.js?v=4.3.186';
+import { updateLoveWidgetUI } from '../we-love/we-love.js?v=4.3.186';
 
 let lastDeletedRecord = null;
 let relationshipChart = null;
