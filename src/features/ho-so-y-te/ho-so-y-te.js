59e4516d9786703a308ef3fff199f1681fda982d
tuaniuminh <tuaniuminh@users.noreply.github.com>
2026-08-28 20:39:17 +0700

    Cap nhat code tu dong

--- src/features/ho-so-y-te/ho-so-y-te.js
+++ src/features/ho-so-y-te/ho-so-y-te.js
@@ -2,8 +2,8 @@ import {
     state, saveLocalState, showToast, performSync,
     APP_VERSION, formatDate, escapeHTML, getLocalDateString,
     callGeminiTextAPI
-} from '../../core/app.js?v=4.3.221';
-import { encrypt, decrypt } from '../../core/crypto.js?v=4.3.221';
+} from '../../core/app.js?v=4.3.222';
+import { encrypt, decrypt } from '../../core/crypto.js?v=4.3.222';
 
 let healthTrendChartInstance = null;
 
--- src/features/ket-noi-gia-dinh/ket-noi.js
+++ src/features/ket-noi-gia-dinh/ket-noi.js
@@ -2,9 +2,9 @@
 import { 
     state, saveLocalState, showToast, performSync,
     escapeHTML, decryptWithPrivateKey
-} from '../../core/app.js?v=4.3.221';
-import { encrypt, decrypt } from '../../core/crypto.js?v=4.3.221';
-import * as sync from '../../core/sync.js?v=4.3.221';
+} from '../../core/app.js?v=4.3.222';
+import { encrypt, decrypt } from '../../core/crypto.js?v=4.3.222';
+import * as sync from '../../core/sync.js?v=4.3.222';
 
 let _pairingInterval = null;
 let _pairingRealtimeChannel = null;
